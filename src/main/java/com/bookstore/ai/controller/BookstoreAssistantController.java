60742b1db4a5b90be50c5a89e8985a8078c91ec8
Vitor Oliveira <vitor.oliveira@digitalscutum.com.br>
2024-04-17 04:50:13 -0300

    commit

---
+++ src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
@@ -0,0 +1,59 @@
+package com.bookstore.ai.controller;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/bookstore")
+public class BookstoreAssistantController {
+
+    private OpenAiChatClient chatClient;
+
+    public BookstoreAssistantController(OpenAiChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    //    @GetMapping("/informations")
+//    public String bookstoreChat(@RequestParam(value = "message",
+//            defaultValue = "Quais os livros best sellers dos ultimos anos?") String message){
+//        return chatClient.call(message);
+//    }
+//    @GetMapping("/stream/informations")
+//    public Flux<String> bookstoreChatStream(@RequestParam(value = "message",
+//            defaultValue = "Quais os livros best sellers dos ultimos anos?") String message){
+//        return chatClient.stream(message);
+//    }
+    @GetMapping("/stream/informations")
+    public Flux<ChatResponse> bookstoreChatStream(@RequestParam(value = "message",
+            defaultValue = "Quais os livros best sellers dos ultimos anos?") String message){
+        return chatClient.stream(new Prompt(message));
+    }
+
+    @GetMapping("/informations")
+    public ChatResponse bookstoreChatEx2(@RequestParam(value = "message",
+            defaultValue = "Quais os livros best sellers dos ultimos anos?") String message){
+        return chatClient.call(new Prompt(message));
+    }
+
+    @GetMapping("/reviews")
+    public String bookstoreReview(@RequestParam(value = "book",
+            defaultValue = "Dom Quixote") String book){
+        PromptTemplate promptTemplate = new PromptTemplate(
+                """
+                Por favor, me forneça
+                um breve resumo do livro {book}
+                e também a biografia de seu autor.
+                """);
+        promptTemplate.add("book", book);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
+    }
+
+
+}
